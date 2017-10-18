792a583fd567ebcc99b9a8d0944941c0e8f594ac
杨振 <yangzhennupt@163.com>
2017-10-18 16:10:17 +0800

    用async控制并发数的爬虫

--- lesson4-wormadvance/app.js
+++ lesson4-wormadvance/app.js
@@ -1,6 +1,61 @@
-var express = require('express');
+//var express = require('express');
 var superagent = require('superagent');
 var cheerio = require('cheerio');
-var ep = require('eventproxy');
-var app = express();
+//var ep = require('eventproxy');
+var url = require('url');
+var async = require('async');
+//var app = express();
+var fs = require('fs');
+
+var cnodeUrl  = 'https://cnodejs.org/';
+var topicContent = [];
+superagent.get(cnodeUrl)
+		.end(function(err,res){
+			if(err){
+				return console.error(err);
+			}
+			var topicUrls=[];
+			var $ = cheerio.load(res.text);
+			 $('#topic_list .topic_title').each(function (index, element) {
+		      		var $element = $(element);
+		      		var href = url.resolve(cnodeUrl, $element.attr('href'));
+		      		topicUrls.push(href);
+   				 });
+			  
+			 async.mapLimit(topicUrls,3,function(url,callback){
+					fetUrl(url,callback)
+				},function(error,result){
+					console.log('final:');
+					console.log(result.length);
+					 
+				 
+					fs.writeFile('./test.json',result,{encoding:'utf-8'},function(err){
+						console.log('saved!');
+					})
+				});
+		})
+
+var fetUrl= function(url,callback){
+	superagent.get(url)
+			.end(function(error,urlRes){
+				if(error){
+					return console.error(error);
+				}
+				var $ = cheerio.load(urlRes.text);
+				var firstComment = $('#content .reply_item').first().find('.markdown-text').text();
+				 // topicContent.push(JSON.stringify({
+				 // 	topicUrl:url,
+				 // 	comment:firstComment
+				 // }))
+				 callback(null,JSON.stringify({
+				 	topicUrl:url,
+				 	comment:firstComment
+				 }))
+				
+			})
+}
+
+// app.listen(3000,function(){
+// 	console.log('server is running at port 3000!');
+// })
 
