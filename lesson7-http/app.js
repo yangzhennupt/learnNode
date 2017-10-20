78f2ceba4a707badce87a70bbfc3097161cde5d7
杨振 <yangzhennupt@163.com>
2017-10-20 15:56:33 +0800

    相互通信的例子，客户端请求数据

--- lesson7-http/app.js
+++ lesson7-http/app.js
@@ -1,11 +1,20 @@
 var http = require('http');
 var qs = require('querystring');
 http.createServer(function(req,res){
+	var body='';
    if(req.url=='/'){
-	   	res.writeHead(200,{'Content-Type':'text/html'});
-	    res.end('<form method="POST" action="/url"><input type="text" name="name"> <button>submit</button></form>');
+   	   req.on('data',function(chunk){
+   	   		body+=chunk;
+   	   });
+   	   req.on('end',function(){
+	   	    res.writeHead(200,{'Content-Type':'text/html'});
+		    
+		   	console.log('get name :  ' + qs.parse(body).name)
+   	   })
+	  
+	    // res.end('<form method="POST" action="/url"><input type="text" name="name"> <button>submit</button></form>');
    }else if(req.url=='/url'&&req.method=="POST"){
-   	var body='';
+   
    	   req.on('data',function(chunk){
    	   		body+=chunk;
    	   });
